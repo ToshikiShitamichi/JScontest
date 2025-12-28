fb96323cd7b8b78401c5aca7f4d52d06b5d9d0f9
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2025-12-28 22:03:00 +0900

    画面設計

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-// $("body").addClass("remove-scrolling");
-// $(".content").hide();
-// $(".start button").on("click", function () {
-//     $(".start").fadeOut(1000);
-//     $(".content").delay(1000).fadeIn(500);
-//     $("body").removeClass("remove-scrolling");
-
-// });
+$(".content").hide();
+$(".start button").on("click", function () {
+    $(".start").fadeOut(1000);
+    $(".content").delay(1000).fadeIn(500);
+});
