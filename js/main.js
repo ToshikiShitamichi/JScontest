e774c52070df528fa0eef5fafb170e65668d8648
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2025-12-29 22:04:08 +0900

    画面遷移完成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$(".content").hide();
-$(".start button").on("click", function () {
-    $(".start").fadeOut(1000);
-    $(".content").delay(1000).fadeIn(500);
+// $(".content").hide();
+// // ①「CPU戦」をクリックしゲーム画面に遷移
+// $(".start button").on("click", function () {
+//     $(".start").fadeOut(1000);
+//     $(".content").delay(1000).fadeIn(500);
+// });
+
+$(".select-num").on("click", function () {
+    const select_num_id = $(this).attr("id");
+    const select_num = select_num_id.substr(4,1) 
+       
+    $(`#${select_num_id}`).attr("disabled","true");
+    console.log(select_num);
+    
 });
